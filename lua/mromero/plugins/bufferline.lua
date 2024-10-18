b2bff59cae844340add01330a25687ca619114f8
Mark Romero <mromero1591@gmail.com>
2024-10-17 23:16:15 -0600

    added new color shceme and upated taps and updated noice

--- lua/mromero/plugins/bufferline.lua
+++ lua/mromero/plugins/bufferline.lua
@@ -1,11 +1,13 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "tabs",
+			show_close_icon = false,
+			show_buffer_close_icons = false,
+		},
+	},
 }
